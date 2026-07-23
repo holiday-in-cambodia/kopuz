f41c03c14c6302362a40fa871cabd97a1cf3c9bc
temidaradev <temidaradev@proton.me>
2026-07-23 23:51:30 +0300

    build: fix bazel for x86_64 linux

--- packaging/nix/shell.nix
+++ packaging/nix/shell.nix
@@ -4,6 +4,9 @@
   mkShell,
   stdenv,
   just,
+  bash,
+  bazelisk,
+  writeShellScriptBin,
   flatpak,
   flatpak-builder,
   appstream,
@@ -16,6 +19,26 @@
 }:
 let
   kopuzPkg = self.packages.${stdenv.hostPlatform.system}.kopuz;
+  nixTarget = lib.replaceStrings [ "-" ] [ "_" ] stdenv.hostPlatform.config;
+  bazel = writeShellScriptBin "bazel" ''
+    case "$1" in
+      build|run|test|coverage|aquery)
+        command="$1"
+        shift
+        exec ${lib.getExe bazelisk} "$command" \
+          --shell_executable=${lib.getExe bash} \
+          --action_env=PATH="$PATH" \
+          --action_env=PKG_CONFIG_PATH="$PKG_CONFIG_PATH" \
+          --action_env=NIX_LDFLAGS="$NIX_LDFLAGS" \
+          --action_env=NIX_CC_WRAPPER_TARGET_HOST_${nixTarget}=1 \
+          --action_env=NIX_BINTOOLS_WRAPPER_TARGET_HOST_${nixTarget}=1 \
+          "$@"
+        ;;
+      *)
+        exec ${lib.getExe bazelisk} "$@"
+        ;;
+    esac
+  '';
 in
 mkShell {
   name = "kopuz-dev";
@@ -24,6 +47,8 @@ mkShell {
   nativeBuildInputs = [
     # Dev
     just
+    bazel
+    bazelisk
 
     nodejs_22
     yt-dlp
